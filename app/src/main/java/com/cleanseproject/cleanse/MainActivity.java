3fb1bff4847e596f02689dfb5bc039a96965093b
julenb <21701719@live.uem.es>
2019-01-23 20:01:53 +0100

    Comenzar actividad

--- app/src/main/java/com/cleanseproject/cleanse/MainActivity.java
+++ app/src/main/java/com/cleanseproject/cleanse/MainActivity.java
@@ -1,5 +1,7 @@
 package com.cleanseproject.cleanse;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -12,11 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imagenHoja = (ImageView)findViewById(R.id.imgHoja);
-        ImageView imagenGota = (ImageView)findViewById(R.id.imgGota);
+        ImageView imagenGota = findViewById(R.id.imgGota);
 
         Animation myanim = AnimationUtils.loadAnimation(this, R.anim.animation_abajo);
         imagenGota.startAnimation(myanim);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
     }
 }
