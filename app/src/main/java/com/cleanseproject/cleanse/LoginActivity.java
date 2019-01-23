b6cbb8ef947aee0fdef5163d7d0ff71d3975036b
julenb <21701719@live.uem.es>
2019-01-23 19:39:43 +0100

    Open email login activity

--- app/src/main/java/com/cleanseproject/cleanse/LoginActivity.java
+++ app/src/main/java/com/cleanseproject/cleanse/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.cleanseproject.cleanse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnGoogle;
+    private Button btnFacebook;
+    private Button btnEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnGoogle = findViewById(R.id.btn_google);
+        btnFacebook = findViewById(R.id.btn_facebook);
+        btnEmail = findViewById(R.id.btn_email);
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, EMailLoginActivity.class));
+            }
+        });
     }
+    
 }
