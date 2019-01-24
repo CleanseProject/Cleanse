79f554ef62ddd63913328ef96feff88370a81808
julenb <21701719@live.uem.es>
2019-01-24 13:50:55 +0100

    Comrpobar email y contraseña

--- app/src/main/java/com/cleanseproject/cleanse/EMailLoginActivity.java
+++ app/src/main/java/com/cleanseproject/cleanse/EMailLoginActivity.java
@@ -2,6 +2,8 @@ package com.cleanseproject.cleanse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,32 +13,70 @@ public class EMailLoginActivity extends AppCompatActivity {
     private Button btnLogIn;
     private EditText txtEMail;
     private EditText txtPassword;
+    private boolean emailCorrecto;
+    private boolean pswdCorrecta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_login);
+        initializeUI();
+    }
+
+    private void initializeUI() {
         btnLogIn = findViewById(R.id.btn_login);
         txtEMail = findViewById(R.id.txt_email);
         txtPassword = findViewById(R.id.txt_pswd);
         btnLogIn.setOnClickListener(v -> logIn(txtEMail.getText().toString(), txtPassword.getText().toString()));
+        txtEMail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                emailCorrecto = checkEMail(s);
+                actualizarBoton();
+            }
+
+            private boolean checkEMail(CharSequence email) {
+                if (email == null)
+                    return false;
+                return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+            }
+        });
+        txtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                pswdCorrecta = !s.toString().equals("");
+                actualizarBoton();
+            }
+        });
+    }
+
+    private void actualizarBoton() {
+        btnLogIn.setEnabled(emailCorrecto && pswdCorrecta);
     }
 
     private void logIn(String email, String password) {
-        if (checkEMail(email) && checkPassword(password)) {
-            // TODO: Iniciar sesión con Firebase
-        }
 
-    }
 
-    private boolean checkEMail(String email) {
-        // TODO: Comprobar si el mail está en blanco, bien formado y mandar advertendia a través de label
-        return false;
-    }
-
-    private boolean checkPassword(String password) {
-        // TODO: Comprobar si se ha insertado una contraseña
-        return false;
     }
 
 
