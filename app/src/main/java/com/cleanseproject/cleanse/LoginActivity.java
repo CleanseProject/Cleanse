e34a0bc5f9d4e6305c481f09010e06533dbf6918
julenb <21701719@live.uem.es>
2019-01-27 17:58:45 +0100

    Google Sign In

--- app/src/main/java/com/cleanseproject/cleanse/LoginActivity.java
+++ app/src/main/java/com/cleanseproject/cleanse/LoginActivity.java
@@ -3,11 +3,20 @@ package com.cleanseproject.cleanse;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button btnGoogle;
+    private SignInButton btnGoogle;
     private Button btnFacebook;
     private Button btnEmail;
 
@@ -15,10 +24,37 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnGoogle = findViewById(R.id.btn_google);
+        btnGoogle = findViewById(R.id.google_sign_in);
+        btnGoogle.setSize(SignInButton.SIZE_STANDARD);
         btnFacebook = findViewById(R.id.btn_facebook);
         btnEmail = findViewById(R.id.btn_email);
         btnEmail.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, EMailLoginActivity.class)));
+        btnGoogle.setOnClickListener(v -> googleSignIn());
+    }
+
+    private void googleSignIn() {
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.firebase_web_client_id))
+                .requestEmail()
+                .build();
+        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(LoginActivity.this, googleSignInOptions);
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 9001);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 9001) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("mail", account.getEmail());
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
 }
