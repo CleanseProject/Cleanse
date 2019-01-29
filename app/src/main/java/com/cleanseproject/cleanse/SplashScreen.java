c5ba1cdb18c221edf9efa5b5ef85222074b76e34
julenb <21701719@live.uem.es>
2019-01-29 17:58:59 +0100

    Cambio de nombre MainActivity a SplashScreen

--- app/src/main/java/com/cleanseproject/cleanse/SplashScreen.java
+++ app/src/main/java/com/cleanseproject/cleanse/SplashScreen.java
@@ -2,7 +2,11 @@ package com.cleanseproject.cleanse;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,6 +14,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-        startActivity(new Intent(this, WelcomeActivity.class));
+        ImageView imagenGota = findViewById(R.id.imgGota);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.animation_abajo);
+        imagenGota.startAnimation(myanim);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, WelcomeActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2500);
     }
 }
