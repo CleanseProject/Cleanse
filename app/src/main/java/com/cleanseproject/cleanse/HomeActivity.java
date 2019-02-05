4eb334cce6f28b6d5dc8d4928c1dab17de80555e
julenb <21701719@live.uem.es>
2019-02-05 16:12:11 +0100

    Activity snackbar on notification

--- app/src/main/java/com/cleanseproject/cleanse/HomeActivity.java
+++ app/src/main/java/com/cleanseproject/cleanse/HomeActivity.java
@@ -1,18 +1,45 @@
 package com.cleanseproject.cleanse;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+
+import com.cleanseproject.cleanse.services.CleanseFirebaseMessagingService;
 
 public class HomeActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        IntentFilter f = new IntentFilter(CleanseFirebaseMessagingService.NOTIFICATION);
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(onEvent, f);
+    }
+
+    private BroadcastReceiver onEvent = new BroadcastReceiver() {
+        public void onReceive(Context ctxt, Intent i) {
+            //if (i.getAction() != null && i.getAction() != MyFirebaseMessagingService.NOTIFICATION) {
+            Log.d("notification", i.getStringExtra("mensaje"));
+            Snackbar.make(findViewById(R.id.homeCoordinatorLayout), i.getStringExtra("mensaje"),
+                    Snackbar.LENGTH_LONG)
+                    .show();
+            //}
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
