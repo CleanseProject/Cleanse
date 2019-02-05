c703ea46243f2d9c5b4d557f8bbee75b46016cda
ruizv <21748226@live.uem.es>
2019-02-05 17:12:38 +0100

    Adaptador de la vistita

--- app/src/main/java/com/cleanseproject/cleanse/HomeActivity.java
+++ app/src/main/java/com/cleanseproject/cleanse/HomeActivity.java
@@ -7,12 +7,15 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class HomeActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private RecyclerView rvEventos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,8 @@ public class HomeActivity extends AppCompatActivity {
             e.printStackTrace();
         }*/
         initializeUI();
+
+
     }
 
     @Override
@@ -52,6 +57,9 @@ public class HomeActivity extends AppCompatActivity {
             drawerLayout.closeDrawers();
             return true;
         });
+
+        rvEventos=findViewById(R.id.rv_Eventos);
     }
+
 
 }
