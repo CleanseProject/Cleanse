e8e511de5fadc1020fcf4080f6bc679f8046c5a0
julenb <21701719@live.uem.es>
2019-02-05 16:02:42 +0100

    Add FCM Service

---
+++ app/src/main/java/com/cleanseproject/cleanse/services/CleanseFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.cleanseproject.cleanse.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class CleanseFirebaseMessagingService extends FirebaseMessagingService {
+}
