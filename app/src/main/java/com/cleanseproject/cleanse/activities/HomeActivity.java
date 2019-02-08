066ed74e1c7f1c6dac88811659af37a8e07cebd9
Adrian <adri.garcia.universidad@gmail.com>
2019-02-08 20:03:11 +0100

    commit vista evento seleccionado con los kilometros

--- app/src/main/java/com/cleanseproject/cleanse/activities/HomeActivity.java
+++ app/src/main/java/com/cleanseproject/cleanse/activities/HomeActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.Fragment;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
