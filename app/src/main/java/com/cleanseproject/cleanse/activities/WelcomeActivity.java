9c053360c3cace525309c05de0157d3c2c54a688
julenb <21701719@live.uem.es>
2019-02-15 18:20:13 +0100

    Request location on start

--- app/src/main/java/com/cleanseproject/cleanse/activities/WelcomeActivity.java
+++ app/src/main/java/com/cleanseproject/cleanse/activities/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package com.cleanseproject.cleanse.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -9,10 +13,19 @@ import com.cleanseproject.cleanse.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 901;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
+        }
     }
 
     public void next(View view) {
