36f5b90185af0086380f912c160daea6b70e9fae
julenb <21701719@live.uem.es>
2019-02-15 16:15:37 +0100

    Convert dialog to activity

---
+++ app/src/main/java/com/cleanseproject/cleanse/activities/AddEventActivity.java
@@ -0,0 +1,15 @@
+package com.cleanseproject.cleanse.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cleanseproject.cleanse.R;
+
+public class AddEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
