f994d7b1715f9601cc00400ee0a8ab59649ba70b
ruizv <21748226@live.uem.es>
2019-02-15 16:32:35 +0100

    SwipeRefresh

--- app/src/main/java/com/cleanseproject/cleanse/fragments/HomeFragment.java
+++ app/src/main/java/com/cleanseproject/cleanse/fragments/HomeFragment.java
@@ -2,6 +2,7 @@ package com.cleanseproject.cleanse.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,7 +24,7 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
 
     private EventManagerService eventManagerService;
-
+    private SwipeRefreshLayout swipeRefresh;
     private AdaptadorRecyclerViews adaptador;
     private RecyclerView rvEventos;
     private ProgressBar progressBar;
@@ -40,12 +41,34 @@ public class HomeFragment extends Fragment {
         View view = getView();
         rvEventos = view.findViewById(R.id.rv_Eventos);
         progressBar=view.findViewById(R.id.home_fragment_pb);
+        swipeRefresh=view.findViewById(R.id.swiperefresh);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                //swipeRefresh.setRefreshing(true);
+
+                updateRecycleView();
+
+            }
+
+
+        });
+
         LinearLayoutManager llm = new GridLayoutManager(getActivity(), 1);
         rvEventos.setLayoutManager(llm);
         eventManagerService = new EventManagerService();
         eventManagerService.getCloseEvents(new GeoLocation(37.7832, -122.4056),
                 10,
                 events -> rellenarEventos(events));
+    }
+
+    private void updateRecycleView() {
+        //Conexion con Firebase para updatear la lista de eventos
+
+
+
+        swipeRefresh.setRefreshing(false);
     }
 
     private void rellenarEventos(ArrayList<Event> events) {
