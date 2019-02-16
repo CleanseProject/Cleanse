35092421380a64e62eb726a7436f7b88ddf68e0f
hernan <hernanagf97@gmail.com>
2019-02-16 16:29:58 +0100

    Modificacion en la actividad de addEvents y en el mapfragment, funcion agregada al boton de select location y un spinner ha sido anadido en el layout de addevent

--- app/src/main/java/com/cleanseproject/cleanse/fragments/MapFragment.java
+++ app/src/main/java/com/cleanseproject/cleanse/fragments/MapFragment.java
@@ -3,11 +3,14 @@ package com.cleanseproject.cleanse.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.cleanseproject.cleanse.R;
+import com.cleanseproject.cleanse.activities.AddEventActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -15,6 +18,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOError;
+import java.io.IOException;
+
+import static android.view.View.GONE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -41,10 +49,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Log.v("Mensaje", latLng+"");
+            }
+        });
     }
 }
