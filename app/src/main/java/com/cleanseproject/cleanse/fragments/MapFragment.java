188f7556133fe49d2ae802986169f1f5c2db48f5
julenb <21701719@live.uem.es>
2019-02-18 16:32:22 +0100

    Show close Events as markers

--- app/src/main/java/com/cleanseproject/cleanse/fragments/MapFragment.java
+++ app/src/main/java/com/cleanseproject/cleanse/fragments/MapFragment.java
@@ -11,6 +11,9 @@ import android.widget.FrameLayout;
 
 import com.cleanseproject.cleanse.R;
 import com.cleanseproject.cleanse.activities.AddEventActivity;
+import com.cleanseproject.cleanse.dataClasses.Event;
+import com.cleanseproject.cleanse.services.EventManagerService;
+import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -21,6 +24,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOError;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static android.view.View.GONE;
 
@@ -30,20 +34,21 @@ import static android.view.View.GONE;
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-    public MapFragment() {
-        // Required empty public constructor
-    }
-
+    private EventManagerService eventManagerService;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        View view = getView();
+        eventManagerService = new EventManagerService();
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
         mapFragment.getMapAsync(this);
-        return view;
     }
 
     @Override
@@ -53,12 +58,17 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                Log.v("Mensaje", latLng+"");
-            }
-        });
+        mMap.setOnMapClickListener(latLng -> Log.v("Mensaje", latLng + ""));
+        eventManagerService.getCloseEvents(new GeoLocation(37.7832, -122.4056),
+                10,
+                this::addEventsToMap);
     }
+
+    private void addEventsToMap(ArrayList<Event> events) {
+        for (Event event : events) {
+            LatLng latLng = new LatLng(Double.parseDouble(event.getLatitude()), Double.parseDouble(event.getLongitude()));
+            mMap.addMarker(new MarkerOptions().position(latLng).title(event.getName()));
+        }
+    }
+
 }
