9a81d41727d185db139565d8652b527c90f74b36
julenb <21701719@live.uem.es>
2019-02-22 17:39:41 +0100

    Save profile pic

--- app/src/main/java/com/cleanseproject/cleanse/services/UserManagerService.java
+++ app/src/main/java/com/cleanseproject/cleanse/services/UserManagerService.java
@@ -1,20 +1,32 @@
 package com.cleanseproject.cleanse.services;
 
+import android.net.Uri;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.net.URI;
 
 public class UserManagerService {
 
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
+    private FirebaseStorage firebaseStorage;
 
     public UserManagerService() {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
+        firebaseStorage = FirebaseStorage.getInstance();
     }
 
-    public void setProfilePhoto(){
-        
+    public void setProfilePhoto(Uri imagePath) {
+        firebaseStorage.getReference("users").child(firebaseUser.getUid())
+                .child("profilePhoto").putFile(imagePath);
+    }
+
+    public void getProfilePhotoDownloadUrl(String userId){
+
     }
 
 }
