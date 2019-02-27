cf3e743ed500cbad279cc9de0046113ac2144f91
Adrian <adri.garcia.universidad@gmail.com>
2019-02-27 18:04:20 +0100

    slider activity (falta mostrarlo tras el splashscreen y ponerlo bonito)

--- app/src/main/java/com/cleanseproject/cleanse/activities/SliderActivity.java
+++ app/src/main/java/com/cleanseproject/cleanse/activities/SliderActivity.java
@@ -1,5 +1,6 @@
 package com.cleanseproject.cleanse.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,7 @@ import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.github.paolorotolo.appintro.model.SliderPage;
 
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 public class SliderActivity extends AppIntro {
 
@@ -18,10 +20,25 @@ public class SliderActivity extends AppIntro {
 
         SliderPage sliderPage = new SliderPage();
         sliderPage.setTitle("¿Qué es Cleanse?");
-        sliderPage.setDescription("Pues hacemos cosas");
         sliderPage.setImageDrawable(R.drawable.imagen);
+        sliderPage.setDescription("Pues hacemos cosas");
         sliderPage.setBgColor(ContextCompat.getColor(getApplicationContext(), R.color.verdehojainterno));
         addSlide(AppIntroFragment.newInstance(sliderPage));
 
+        SliderPage sliderPage2 = new SliderPage();
+        sliderPage2.setTitle("¿Qué harás con nosotros?");
+        sliderPage2.setImageDrawable(R.drawable.imagen);
+        sliderPage2.setDescription("Quedaras y todo eso con gente y tal");
+        sliderPage2.setBgColor(ContextCompat.getColor(getApplicationContext(), R.color.verdehojainterno));
+        addSlide(AppIntroFragment.newInstance(sliderPage2));
+
+        showSkipButton(false);
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        Intent intent = new Intent(getApplicationContext(),UserDataActivity.class);
+        startActivity(intent);
     }
 }
