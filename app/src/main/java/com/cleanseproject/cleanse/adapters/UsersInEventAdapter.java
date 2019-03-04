6e3f35b5adda732cb6509e612e7ffcc6a429bc82
hernan <hernanagf97@gmail.com>
2019-03-04 18:45:11 +0100

    usuario que integran el evento con foto

--- app/src/main/java/com/cleanseproject/cleanse/adapters/UsersInEventAdapter.java
+++ app/src/main/java/com/cleanseproject/cleanse/adapters/UsersInEventAdapter.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.cleanseproject.cleanse.R;
 import com.cleanseproject.cleanse.callbacks.ImageUrlLoadCallback;
 import com.cleanseproject.cleanse.dataClasses.User;
@@ -61,12 +63,8 @@ public class UsersInEventAdapter extends RecyclerView.Adapter<UsersInEventAdapte
         public void bind(User user) {
             // TODO: Cambiar por la foto de User en Firebase
             lblUsername.setText(user.getName());
-           /* ivUser.setImageURI(new ImageManagerService().userImageDownloadUrl(user.getUserId(), new ImageUrlLoadCallback() {
-                @Override
-                public void onUrlLoaded(String url) {
-                    ivUser.setImageURI(url);
-                }
-            }));*/
+            new ImageManagerService().userImageDownloadUrl(user.getUserId(), url
+                    -> Glide.with(context).load(url).apply(RequestOptions.centerCropTransform().circleCropTransform()).into(ivUser));
         }
     }
 }
